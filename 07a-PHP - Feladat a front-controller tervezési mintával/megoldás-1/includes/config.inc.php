6fe2204e9ce3f730db7a162905c37b78dfde615c
NemRohogEggyutterez <matetakacs05@gmail.com>
2026-04-23 15:26:09 +0200

    mintaoldal betoltve, adatbazis osszekapcsolva: belepes es regisztracio mukodik

---
+++ 07a-PHP - Feladat a front-controller tervezési mintával/megoldás-1/includes/config.inc.php
@@ -0,0 +1,27 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Mini honlap Kft.',
+);
+
+$fejlec = array(
+    'kepforras' => 'logo.png',
+    'kepalt' => 'logo',
+	'cim' => 'Mini honlap',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'Mini honlap Kft.'
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Címlap'),
+	'bemutatkozas' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás'),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat'),
+	'cikkek' => array('fajl' => 'cikkek', 'szoveg' => 'Cikkek'),
+    'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat')
+);
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
