4db7eaa17206e6705d5c500a632fce84674a1f41
NemRohogEggyutterez <matetakacs05@gmail.com>
2026-04-28 18:52:06 +0200

    css-hez szukseges div-ek hozzaadva a hattervideo berakva

--- templates/pages/kapcsolat.tpl.php
+++ templates/pages/kapcsolat.tpl.php
@@ -1,16 +1,26 @@
 <?php require "includes/db.php"; ?>
-<h2>Adatok:</h2>
-<p>Ügyvezető: <strong>Valaki Az</strong></p>
-<p>E-mail: <strong>valaki.az@minihonlap.hu</strong></p>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2726.3375296155727!2d19.66695091525771!3d46.89607994478184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4743da7a6c479e1d%3A0xc8292b3f6dc69e7f!2sPallasz+Ath%C3%A9n%C3%A9+Egyetem+GAMF+Kar!5e0!3m2!1shu!2shu!4v1475753185783" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-<br>
-<a target="_blank" href="https://www.google.hu/maps/place/Pallasz+Ath%C3%A9n%C3%A9+Egyetem+GAMF+Kar/@46.8960799,19.6669509,17z/data=!3m1!4b1!4m5!3m4!1s0x4743da7a6c479e1d:0xc8292b3f6dc69e7f!8m2!3d46.8960763!4d19.6691396?hl=hu">Nagyobb térkép</a>
-<form method="POST" id="uzenetForm">
-    <label for="szoveg">Üzenet:</label><br>
-    <textarea name="szoveg" id="szoveg"></textarea>
-    <br><br>
-    <button type="submit">Küldés</button>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <video autoplay muted loop playsinline id="bg-video">
+        <source src="../bgv.mp4" type="video/mp4" />
+    </video>
+    <div class="contentkap">
+        <h1>Üzenet Küldése</h1>
+        <form method="POST" class="uzenetForm">
+            <label for="szoveg">Üzenet:</label><br>
+            <textarea name="szoveg" id="szoveg" placeholder="Üzeneted:"></textarea>
+            <br><br>
+        <button type="submit">Küldés</button>
+    </div>
 </form>
+</body>
+</html>
 <script>
 document.getElementById('uzenetForm').onsubmit = function(e) {
     var szoveg = document.getElementById('szoveg').value.trim();
